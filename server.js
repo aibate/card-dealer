372dc59ada06186a08d4ddfacfafa7cd560d5fbd
aibate <indigoambe@gmail.com>
2022-10-15 14:27:42 +1100

    setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const { application, response } = require('express');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.static('./client/build'));
+
+app.get("/api/test", (req, res) => {
+    res.json({ result: "success" });
+  });
+
+app.listen(port, ()=> console.log(`Listening on ${port}`));
